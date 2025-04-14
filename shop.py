c9efe0ed830ad75316ef622e6f7990f4a8e695d4
KumarSam <sampank@gmail.com>
2025-04-14 18:36:03 +0100

    Shop samarth

--- shop.py
+++ shop.py
@@ -6,7 +6,7 @@ toys = ["teddy bears", "puzzles",andvariable,"scrabble"]
 phones = ["samsung phones","apple phones",andvariable,"motorola phones"]
 stationary = ["notebooks","pens",andvariable,"pencils"]
 shop = (toys[ : 3],phones[ : 3],stationary [ : 3])
-what_customer_origanally_wants = input("What do you want. We have toys, phones, and stationery, ").casefold()
+what_customer_origanally_wants = input("What do you want, we have toys, phones, and stationery? ").casefold()
 if what_customer_origanally_wants == "toys":
     print("We have" + str(toys))
     choice = input("Which one do you want?").casefold()
@@ -24,7 +24,7 @@ elif what_customer_origanally_wants == "phones":
 elif what_customer_origanally_wants == "stationary":
     print("We have" + str(stationary))
     choice = input("Which one do you want?" ).casefold()
-    if choice in toys:
+    if choice in stationary:
         print("OK," + choice + " is for " + str(stationaryprice) + " pounds")
     else:
         print("Sorry.We don't have that")
