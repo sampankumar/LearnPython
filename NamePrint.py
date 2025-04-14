459461fe491476a00bc8ce154e3ff923a95af34c
KumarSam <sampank@gmail.com>
2025-04-14 15:02:25 +0100

    MY LOCKED IN DUNGEON GAME

--- NamePrint.py
+++ NamePrint.py
@@ -1,5 +1,5 @@
 print("You are locked in a dungeon, you need to get out by choosing 1, 2, 3 or 4")
-first_choice = input("Enter your first choice")
+first_choice = input("Enter your choice: ")
 if first_choice == "1":
     print("oh no! You get eaten by a dragon! Game Over")
 elif first_choice == "2":
@@ -9,10 +9,10 @@ elif first_choice == "4":
 elif first_choice == "3":
     print("Well done! You exited safely!")
 if first_choice != "3":
-    replay_choice = input("Do you want to try again? Enter 'replay' if you do")
+    replay_choice = input("Do you want to try again? Enter 'replay' if you do: ")
     if replay_choice == "replay":
         print("You are locked in a dungeon, you need to get out by choosing 1, 2, 3 or 4")
-        first_choice = input("Enter your first choice")
+        first_choice = input("Enter your choice: ")
         if first_choice == "1":
             print("oh no! You get eaten by a dragon! Game Over")
         elif first_choice == "2":
